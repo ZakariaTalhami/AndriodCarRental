f23e327b21dcbef9814b4186d1be9f1dee06908a
mShallakh <mShallakh@users.noreply.github.com>
2018-12-17 15:56:13 +0200

    ContactUS and HomePage

--- app/src/main/java/com/rental/shaltal/carrental/MainPage.java
+++ app/src/main/java/com/rental/shaltal/carrental/MainPage.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,6 +17,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.rental.shaltal.carrental.helpers.SharedPrefHelper;
@@ -144,6 +148,15 @@ public class MainPage extends AppCompatActivity
     }
 
     private void showHome() {
+
+        FrameLayout mylayout = (FrameLayout) findViewById(R.id.frag_container);
+        mylayout.removeAllViews();
+
+        HomePage_Fragment homePage = new HomePage_Fragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frag_container, homePage,"HomePage");
+        fragmentTransaction.commit();
     }
 
     private void showProfile() {
@@ -162,7 +175,14 @@ public class MainPage extends AppCompatActivity
     }
 
     private void showContactPage() {
+        FrameLayout mylayout = (FrameLayout) findViewById(R.id.frag_container);
+        mylayout.removeAllViews();
 
+        ContactUsFragment ContactUs = new ContactUsFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frag_container, ContactUs,"ContactUs");
+        fragmentTransaction.commit();
     }
 
     private void goToLoginPage(){
